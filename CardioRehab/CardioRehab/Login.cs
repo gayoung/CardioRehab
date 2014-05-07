6a2be90dd32802e1e7fc6f9cad88ebe2b740a85d
Ga Young Kim <gayoung1@ualberta.ca>
2014-05-07 17:54:48 -0600

    now login is connected to two diff screens depending on the user's role

--- CardioRehab/CardioRehab/Login.cs
+++ CardioRehab/CardioRehab/Login.cs
@@ -48,16 +48,25 @@ namespace CardioRehab
                     while (reader.Read())
                     {
                         String role = reader["role"].ToString();
+                        int userid = int.Parse(reader["id"].ToString());
 
                         if (role.Trim() == "Patient") // user is a patient
                         {
                             Console.WriteLine("authenticated user is a patient");
                             // open patient window
+                            PatientMain patientWindow = new PatientMain(userid);
+                            patientWindow.Show();
+                            patientWindow.FormClosed += new FormClosedEventHandler(MainWindowClosed);
+                            this.Hide();
                         }
                         else if (role.Trim() == "Doctor") // user is a doctor
                         {
                             Console.WriteLine("authenticated user is a doctor");
                             // open doctor window
+                            DoctorMain doctorWindow = new DoctorMain(userid);
+                            doctorWindow.Show();
+                            doctorWindow.FormClosed += new FormClosedEventHandler(MainWindowClosed);
+                            this.Hide();
                         }
                         else // user is an admin
                         {
@@ -81,6 +90,12 @@ namespace CardioRehab
             
             cmd.Dispose();
             db.m_dbconnection.Close();
+        }
+
+        // need this function to close the hidden login form
+        private void MainWindowClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
