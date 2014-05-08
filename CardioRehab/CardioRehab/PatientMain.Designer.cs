2394dce8bc1d22bb227a3e165c4e9c1c3109ae75
Ga Young Kim <gayoung1@ualberta.ca>
2014-05-08 13:33:28 -0600

    made the patient UI resizable

--- CardioRehab/CardioRehab/PatientMain.Designer.cs
+++ CardioRehab/CardioRehab/PatientMain.Designer.cs
@@ -30,57 +30,150 @@ namespace CardioRehab
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PatientMain));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.doctorPanel = new System.Windows.Forms.Panel();
+            this.patientPanel = new System.Windows.Forms.Panel();
+            this.hrLabel = new System.Windows.Forms.Label();
+            this.BiostatPanel = new System.Windows.Forms.Panel();
+            this.oxiValue = new System.Windows.Forms.Label();
+            this.oxiLabel = new System.Windows.Forms.Label();
+            this.bpValue = new System.Windows.Forms.Label();
+            this.bpLabel = new System.Windows.Forms.Label();
+            this.hrValue = new System.Windows.Forms.Label();
+            this.BiostatPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(609, 587);
+            this.panel1.Size = new System.Drawing.Size(586, 587);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // doctorPanel
             // 
-            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel2.Location = new System.Drawing.Point(642, 12);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(176, 151);
-            this.panel2.TabIndex = 1;
+            this.doctorPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.doctorPanel.Location = new System.Drawing.Point(616, 12);
+            this.doctorPanel.Name = "doctorPanel";
+            this.doctorPanel.Size = new System.Drawing.Size(202, 151);
+            this.doctorPanel.TabIndex = 1;
             // 
-            // panel3
+            // patientPanel
             // 
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel3.Location = new System.Drawing.Point(642, 169);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(176, 151);
-            this.panel3.TabIndex = 2;
+            this.patientPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.patientPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.patientPanel.Location = new System.Drawing.Point(616, 169);
+            this.patientPanel.Name = "patientPanel";
+            this.patientPanel.Size = new System.Drawing.Size(202, 151);
+            this.patientPanel.TabIndex = 2;
             // 
-            // splitter1
+            // hrLabel
             // 
-            this.splitter1.Location = new System.Drawing.Point(0, 0);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 616);
-            this.splitter1.TabIndex = 3;
-            this.splitter1.TabStop = false;
+            this.hrLabel.AutoSize = true;
+            this.hrLabel.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.hrLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hrLabel.Image = ((System.Drawing.Image)(resources.GetObject("hrLabel.Image")));
+            this.hrLabel.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.hrLabel.Location = new System.Drawing.Point(3, 17);
+            this.hrLabel.Name = "hrLabel";
+            this.hrLabel.Size = new System.Drawing.Size(147, 26);
+            this.hrLabel.TabIndex = 0;
+            this.hrLabel.Text = "     Heart Rate";
+            this.hrLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // BiostatPanel
+            // 
+            this.BiostatPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BiostatPanel.BackColor = System.Drawing.SystemColors.Menu;
+            this.BiostatPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.BiostatPanel.Controls.Add(this.oxiValue);
+            this.BiostatPanel.Controls.Add(this.oxiLabel);
+            this.BiostatPanel.Controls.Add(this.bpValue);
+            this.BiostatPanel.Controls.Add(this.bpLabel);
+            this.BiostatPanel.Controls.Add(this.hrValue);
+            this.BiostatPanel.Controls.Add(this.hrLabel);
+            this.BiostatPanel.Location = new System.Drawing.Point(616, 336);
+            this.BiostatPanel.Name = "BiostatPanel";
+            this.BiostatPanel.Size = new System.Drawing.Size(202, 263);
+            this.BiostatPanel.TabIndex = 4;
+            // 
+            // oxiValue
+            // 
+            this.oxiValue.AutoSize = true;
+            this.oxiValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.oxiValue.Location = new System.Drawing.Point(72, 172);
+            this.oxiValue.Name = "oxiValue";
+            this.oxiValue.Size = new System.Drawing.Size(26, 26);
+            this.oxiValue.TabIndex = 6;
+            this.oxiValue.Text = "--";
+            // 
+            // oxiLabel
+            // 
+            this.oxiLabel.AutoSize = true;
+            this.oxiLabel.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.oxiLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.oxiLabel.Image = ((System.Drawing.Image)(resources.GetObject("oxiLabel.Image")));
+            this.oxiLabel.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.oxiLabel.Location = new System.Drawing.Point(3, 139);
+            this.oxiLabel.Name = "oxiLabel";
+            this.oxiLabel.Size = new System.Drawing.Size(156, 26);
+            this.oxiLabel.TabIndex = 5;
+            this.oxiLabel.Text = "     Oxygen Sat";
+            this.oxiLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // bpValue
+            // 
+            this.bpValue.AutoSize = true;
+            this.bpValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bpValue.Location = new System.Drawing.Point(72, 107);
+            this.bpValue.Name = "bpValue";
+            this.bpValue.Size = new System.Drawing.Size(26, 26);
+            this.bpValue.TabIndex = 4;
+            this.bpValue.Text = "--";
+            // 
+            // bpLabel
+            // 
+            this.bpLabel.AutoSize = true;
+            this.bpLabel.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.bpLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bpLabel.Image = ((System.Drawing.Image)(resources.GetObject("bpLabel.Image")));
+            this.bpLabel.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.bpLabel.Location = new System.Drawing.Point(3, 79);
+            this.bpLabel.Name = "bpLabel";
+            this.bpLabel.Size = new System.Drawing.Size(191, 26);
+            this.bpLabel.TabIndex = 3;
+            this.bpLabel.Text = "     Blood Pressure";
+            this.bpLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // hrValue
+            // 
+            this.hrValue.AutoSize = true;
+            this.hrValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hrValue.Location = new System.Drawing.Point(72, 43);
+            this.hrValue.Name = "hrValue";
+            this.hrValue.Size = new System.Drawing.Size(26, 26);
+            this.hrValue.TabIndex = 2;
+            this.hrValue.Text = "--";
             // 
             // PatientMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(830, 616);
-            this.Controls.Add(this.splitter1);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(829, 611);
+            this.Controls.Add(this.BiostatPanel);
+            this.Controls.Add(this.patientPanel);
+            this.Controls.Add(this.doctorPanel);
             this.Controls.Add(this.panel1);
             this.Name = "PatientMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Welcome to CardioRehab Application!";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.BiostatPanel.ResumeLayout(false);
+            this.BiostatPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -88,8 +181,14 @@ namespace CardioRehab
         #endregion
 
         private Panel panel1;
-        private Panel panel2;
-        private Panel panel3;
-        private Splitter splitter1;
+        private Panel doctorPanel;
+        private Panel patientPanel;
+        private Label hrLabel;
+        private Panel BiostatPanel;
+        private Label hrValue;
+        private Label bpLabel;
+        private Label bpValue;
+        private Label oxiLabel;
+        private Label oxiValue;
     }
 }
--- CardioRehab/CardioRehab/PatientMain.cs
+++ CardioRehab/CardioRehab/PatientMain.cs
@@ -17,6 +17,70 @@ namespace CardioRehab
             int user = currentuser;
 
             InitializeComponent();
+
+            this.SizeChanged += new EventHandler(PatientMain_Resize);
         }
+
+        private void PatientMain_Resize(object sender, EventArgs e)
+        {
+            int currentWidth = this.Width;
+            int currentHeight = this.Height;
+
+            panel1.Width = (int)(currentWidth * 0.70);
+            panel1.Height = (int)(currentHeight * 0.9);
+
+            int newdoctorPanelX = panel1.Location.X + panel1.Width + 10;
+            int doctorPanelY = panel1.Location.Y;
+
+            Console.WriteLine("doctorPanelY: " + doctorPanelY.ToString());
+
+            doctorPanel.Location = new Point(newdoctorPanelX, doctorPanelY);
+
+            doctorPanel.Width = (int)(currentWidth * 0.25);
+            doctorPanel.Height = (int)(currentHeight * 0.23);
+
+            int patientPanelY = doctorPanelY + doctorPanel.Height + 10;
+
+            patientPanel.Location = new Point(newdoctorPanelX, patientPanelY);
+
+            patientPanel.Width = (int)(currentWidth * 0.25);
+            patientPanel.Height = (int)(currentHeight * 0.23);
+
+            int BiostatPanellY = patientPanelY + patientPanel.Height + 10;
+
+            BiostatPanel.Location = new Point(newdoctorPanelX, BiostatPanellY);
+
+            BiostatPanel.Width = (int)(currentWidth * 0.25);
+            BiostatPanel.Height = (int)(currentHeight * 0.42);
+
+            if(WindowState == FormWindowState.Maximized)
+            {
+                Font newStyle = new Font(hrLabel.Font.FontFamily, 22);
+                hrLabel.Font = newStyle;
+                hrValue.Font = newStyle;
+                bpLabel.Font = newStyle;
+                bpValue.Font = newStyle;
+                oxiLabel.Font = newStyle;
+                oxiValue.Font = newStyle;
+
+            }
+            else
+            {
+                Font newStyle = new Font(hrLabel.Font.FontFamily, 16);
+                hrLabel.Font = newStyle;
+                hrValue.Font = newStyle;
+                bpLabel.Font = newStyle;
+                bpValue.Font = newStyle;
+                oxiLabel.Font = newStyle;
+                oxiValue.Font = newStyle;
+            }
+
+            hrValue.Location = new Point(hrValue.Location.X, hrLabel.Location.Y + hrLabel.Height + 5);
+            bpLabel.Location = new Point(bpLabel.Location.X, hrValue.Location.Y + hrValue.Height + 5);
+            bpValue.Location = new Point(bpValue.Location.X, bpLabel.Location.Y + bpLabel.Height + 5);
+            oxiLabel.Location = new Point(oxiLabel.Location.X, bpValue.Location.Y + bpValue.Height + 5);
+            oxiValue.Location = new Point(oxiValue.Location.X, oxiLabel.Location.Y + oxiLabel.Height + 5);
+        }
+
     }
 }
