c132c793a5690ca71d57fd192ca8ffc329d0dace
Ga Young Kim <gayoung1@ualberta.ca>
2014-05-07 18:06:08 -0600

    starting to build patient UI

--- CardioRehab/CardioRehab/PatientMain.Designer.cs
+++ CardioRehab/CardioRehab/PatientMain.Designer.cs
@@ -30,13 +30,53 @@ namespace CardioRehab
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.splitter1 = new System.Windows.Forms.Splitter();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(609, 587);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Location = new System.Drawing.Point(642, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(176, 151);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Location = new System.Drawing.Point(642, 169);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(176, 151);
+            this.panel3.TabIndex = 2;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 616);
+            this.splitter1.TabIndex = 3;
+            this.splitter1.TabStop = false;
             // 
             // PatientMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(830, 616);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "PatientMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Welcome to CardioRehab Application!";
@@ -46,5 +86,10 @@ namespace CardioRehab
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Splitter splitter1;
     }
 }
