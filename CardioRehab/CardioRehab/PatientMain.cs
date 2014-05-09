dd26a3701b36a78dc6f758606b7b611eee3db08b
Ga Young Kim <gayoung1@ualberta.ca>
2014-05-09 11:43:23 -0600

    now can get bluetooth data from the phone

--- CardioRehab/CardioRehab/PatientMain.cs
+++ CardioRehab/CardioRehab/PatientMain.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -15,32 +16,74 @@ namespace CardioRehab
 {
     public partial class PatientMain : Form
     {
+        private int user;
         // currently under assumption that
         // first output from the loop is LAN and second is wireless
         private String docLocalIp;
+        private String patientLocalIp;
         private String wirelessIP;
 
         private AsyncCallback socketBioWorkerCallback;
         public Socket socketBioListener;
         public Socket bioSocketWorker;
-        static string fname = string.Format("Tiny Tim-{0:yyyy-MM-dd hh.mm.ss.tt}.txt", DateTime.Now);
+        //static string fname = string.Format("Tiny Tim-{0:yyyy-MM-dd hh.mm.ss.tt}.txt", DateTime.Now);
 
         int[] oxdata = new int[1000];
         int[] hrdata = new int[1000];
         int[] bpdata = new int[1000];
         public int hrcount, oxcount, bpcount;
 
+        /// <summary>
+        /// Constructor for this class
+        /// </summary>
+        /// <param name="currentuser"> database ID for the current user</param>
         public PatientMain(int currentuser)
         {
-            int user = currentuser;
+            user = currentuser;
 
+            GetLocalIP();
+            CheckRecord();
             InitializeComponent();
             //used to start socket server for bioSockets
             InitializeBioSockets();
+            MessageBox.Show("Please enter the following IP address to the phone: " + wirelessIP);
 
             this.SizeChanged += new EventHandler(PatientMain_Resize);
         }
-        
+
+        #region Helper functions
+
+        private void CheckRecord()
+        {
+            DatabaseClass db = new DatabaseClass();
+            try
+            {
+                db.m_dbconnection.Open();
+            }
+            catch (FileLoadException error)
+            {
+                // db connection failed
+                MessageBox.Show(error.Message);
+            }
+
+            String query = "SELECT wireless_ip FROM patient WHERE patient_id=" + user;
+            SQLiteCommand cmd = new SQLiteCommand(query, db.m_dbconnection);
+           
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            // correct username and password
+            if (!reader.HasRows)
+            {
+                String updatesql = "UPDATE patient SET wireless_ip="+ wirelessIP +
+                    ", local_ip=" + patientLocalIp + " WHERE patient_id=" + user;
+
+                SQLiteCommand updatecmd = new SQLiteCommand(updatesql, db.m_dbconnection);
+                updatecmd.ExecuteNonQuery();
+
+            }
+            reader.Dispose();
+        }
+
         /// <summary>
         /// This method is used to adjust the size of the form components
         /// when the form window is resized (min/max).
@@ -58,24 +101,19 @@ namespace CardioRehab
             int newdoctorPanelX = panel1.Location.X + panel1.Width + 10;
             int doctorPanelY = panel1.Location.Y;
 
-            Console.WriteLine("doctorPanelY: " + doctorPanelY.ToString());
-
             doctorPanel.Location = new Point(newdoctorPanelX, doctorPanelY);
-
             doctorPanel.Width = (int)(currentWidth * 0.25);
             doctorPanel.Height = (int)(currentHeight * 0.23);
 
             int patientPanelY = doctorPanelY + doctorPanel.Height + 10;
 
             patientPanel.Location = new Point(newdoctorPanelX, patientPanelY);
-
             patientPanel.Width = (int)(currentWidth * 0.25);
             patientPanel.Height = (int)(currentHeight * 0.23);
 
             int BiostatPanellY = patientPanelY + patientPanel.Height + 10;
 
             BiostatPanel.Location = new Point(newdoctorPanelX, BiostatPanellY);
-
             BiostatPanel.Width = (int)(currentWidth * 0.25);
             BiostatPanel.Height = (int)(currentHeight * 0.42);
 
@@ -108,41 +146,59 @@ namespace CardioRehab
             oxiValue.Location = new Point(oxiValue.Location.X, oxiLabel.Location.Y + oxiLabel.Height + 5);
         }
 
-        #region localIP
+        /// <summary>
+        /// This method is used to get both LAN and wireless IP of the current user
+        /// </summary>
         private void GetLocalIP()
         {
-            Console.WriteLine(Dns.GetHostName());
             IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
             int Ipcounter = 0;
             foreach (IPAddress addr in localIPs)
             {
                 if (addr.AddressFamily == AddressFamily.InterNetwork)
                 {
-                    if (Ipcounter == 1)
+                    if(Ipcounter == 0)
+                    {
+                        patientLocalIp = addr.ToString();
+                    }
+                    else if(Ipcounter == 1)
                     {
                         wirelessIP = addr.ToString();
                     }
+                    Ipcounter++;
                 }
             }
         }
+
+        public static String GetTimestamp(DateTime value)
+        {
+            return value.ToString("HH:mm:ss");
+        }
+
         #endregion
 
+        #region Setting up the socket connection for bio information
         private void InitializeBioSockets()
         {
             try
             {
                 //create listening socket
                 socketBioListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                //IPAddress addy = System.Net.IPAddress.Parse("127.0.0.1");
-                //IPEndPoint iplocal = new IPEndPoint(addy, 4444);
-                IPAddress addy = System.Net.IPAddress.Parse(wirelessIP);
-                IPEndPoint iplocal = new IPEndPoint(addy, 4444);
-                //bind to local IP Address
-                socketBioListener.Bind(iplocal);
-                //start listening -- 4 is max connections queue, can be changed
-                socketBioListener.Listen(4);
-                //create call back for client connections -- aka maybe recieve video here????
-                socketBioListener.BeginAccept(new AsyncCallback(OnBioSocketConnection), null);
+                if (wirelessIP != null)
+                {
+                    IPAddress addy = System.Net.IPAddress.Parse(wirelessIP);
+                    IPEndPoint iplocal = new IPEndPoint(addy, 4444);
+                    //bind to local IP Address
+                    socketBioListener.Bind(iplocal);
+                    //start listening -- 4 is max connections queue, can be changed
+                    socketBioListener.Listen(4);
+                    //create call back for client connections -- aka maybe recieve video here????
+                    socketBioListener.BeginAccept(new AsyncCallback(OnBioSocketConnection), null);
+                }
+                else
+                {
+                    MessageBox.Show("No wireless signal detected. Please connect the computer to a wireless network.");
+                }
 
             }
             catch (SocketException e)
@@ -157,10 +213,10 @@ namespace CardioRehab
         {
 
             //BT creates file
-            using (StreamWriter sw = new StreamWriter(File.Create(fname)))
-            {
-                sw.WriteLine("Session started.");
-            }
+            //using (StreamWriter sw = new StreamWriter(File.Create(fname)))
+            //{
+            //    sw.WriteLine("Session started.");
+            //}
             //*
 
             try
@@ -217,12 +273,6 @@ namespace CardioRehab
                 System.String tmp = new System.String(chars);
                 //MessageBox.Show(tmp);
 
-                //BT time stamp
-                long ticks = DateTime.UtcNow.Ticks - DateTime.Parse("01/01/1970 00:00:00").Ticks;
-                ticks /= 10000000; //Convert windows ticks to seconds
-                string timestamp = ticks.ToString();
-                //*
-
                 if (!tmp.Contains('|'))
                 {
                     // MessageBox.Show(tmp);
@@ -231,13 +281,13 @@ namespace CardioRehab
                 }
                 System.String[] name = tmp.Split('|');
 
-                //System.String[] fakeBP = new String[1] { "BP" };
                 System.String[] fakeECG = new String[1] { "ECG" };
 
 
                 if (name.Length == 2)
                 {
                     System.String[] data = name[1].Split(' ');
+                    String timeStamp = GetTimestamp(DateTime.Now);
 
                     byte[] dataToClinician = System.Text.Encoding.ASCII.GetBytes(tmp);
 
@@ -248,45 +298,47 @@ namespace CardioRehab
                     if (data[0] == "HR")
                     {
                         //BT
-                        String timeStamp = GetTimestamp(DateTime.Now);
                         hrdata[hrcount] = Convert.ToInt32(data[1]);
                         hrcount++;
-                        using (StreamWriter sw = File.AppendText(fname))
-                        {
-                            sw.WriteLine(timeStamp + " |" + "HR " + data[1]);
-                        }
+                        //using (StreamWriter sw = File.AppendText(fname))
+                        //{
+                        //    sw.WriteLine(timeStamp + " |" + "HR " + data[1]);
+                        //}
                         //*
 
                         // Below target zone.
-                        hrValue.Text = data[1] + "bpm";
+                        hrValue.Invoke((MethodInvoker)(() => hrValue.Text = data[1] + " bpm"));
                     }
 
                     // Change the Sats display in the UI thread.
                     else if (data[0] == "OX")
                     {
                         //BT
-                        String timeStamp = GetTimestamp(DateTime.Now);
                         oxdata[oxcount] = Convert.ToInt32(data[1]); ;
                         oxcount++;
-                        using (StreamWriter sw = File.AppendText(fname))
+                        //using (StreamWriter sw = File.AppendText(fname))
+                        //{
+                        //    sw.WriteLine(timeStamp + " |" + "OX " + data[1]);
+                        //}
+
+                        // MethodInvoker had to be used to solve cross threading issue
+                        if(data[1] != null && data[2] != null)
                         {
-                            sw.WriteLine(timeStamp + " |" + "OX " + data[1]);
+                            oxiValue.Invoke((MethodInvoker)(() => oxiValue.Text = data[1] + " %"));
+                            hrValue.Invoke((MethodInvoker)(() => hrValue.Text = data[2] + " bpm"));
                         }
-                        
-                        oxiValue.Text = data[1] + " %";
                     }
 
                     if (data[0] == "BP")
                     {
                         //BT
-                        String timeStamp = GetTimestamp(DateTime.Now);
                         bpdata[bpcount] = Convert.ToInt32(data[1]); ;
                         bpcount++;
-                        using (StreamWriter sw = File.AppendText(fname))
-                        {
-                            sw.WriteLine(timeStamp + " |" + "BP " + data[1]);
-                        }
-                        bpValue.Text = data[1] + "/" + data[2];
+                        //using (StreamWriter sw = File.AppendText(fname))
+                        //{
+                        //    sw.WriteLine(timeStamp + " |" + "BP " + data[1]);
+                        //}
+                        bpValue.Invoke((MethodInvoker)(() => bpValue.Text = data[1] + "/" + data[2]));
                     }
                 }
                 WaitForBioData(bioSocketWorker);
@@ -303,10 +355,7 @@ namespace CardioRehab
 
         }
 
-        public static String GetTimestamp(DateTime value)
-        {
-            return value.ToString("HH:mm:ss");
-        }
+        #endregion
 
     }
 }
